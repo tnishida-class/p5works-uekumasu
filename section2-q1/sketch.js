e2ee2c3f425be11b89fe0ae969e0a54579f909de
uekumasu <72775580+uekumasu@users.noreply.github.com>
2020-10-27 12:08:52 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,13 @@
 // 小手調べ
 function setup(){
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    if(i < 5){
+      stroke(255, 0, 0);
+    }
+    else{
+      stroke(0, 0, 255);
+    }
+  x = 100 - (i * 10)
+  ellipse(50, 50 , x)
   }
 }
